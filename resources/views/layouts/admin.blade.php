06750741fa69bc0c2f02a643e1f2edbf76fc7ded
Jhon Quiceno <quicenojale@gmail.com>
2025-11-17 08:38:07 -0500

    refactor(layouts): actualizar layout admin para Clínica Familiar - Incluir CSS personalizado clinica-familiar.css - Mantener estructura base - Preparar para panel admin rediseñado

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -232,12 +232,17 @@ scratch. This page gets rid of all links and provides the needed markup only.
                     </li>
 
                     <li class="nav-item">
-                        <a href="#" class="nav-link" style="background-color: #a9200e">
+                        <a href="{{ route('logout') }}" class="nav-link" style="background-color: #a9200e" id=""
+                           onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
                             <i class="nav-icon fas bi bi-door-closed"></i>
                             <p>
                                 Cerrar Sesión
                             </p>
                         </a>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                            @csrf
+                        </form>
                     </li>
                 </ul>
             </nav>
