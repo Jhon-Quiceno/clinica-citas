20ee25deb0dccb46c42b309183525165614f480e
Jhon Quiceno <quicenojale@gmail.com>
2025-11-16 22:15:06 -0500

    ui(admin): add navigation links for clinic modules in admin panel

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -49,5 +49,47 @@
             </div>
         </div>
 
+        <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-warning">
+                <div class="inner">
+                    <h3>{{ $total_consultorios }}</h3>
+                    <p>Consultorios</p>
+                </div>
+                <div class="icon">
+                    <i class="ion fas bi bi-door-open"></i>
+                </div>
+                <a href="{{ url('admin/consultorios') }}" class="small-box-footer">Más información<i class="fas bi bi-file-person"></i></a>
+            </div>
+        </div>
+
+        <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-danger">
+                <div class="inner">
+                    <h3>{{ $total_doctores }}</h3>
+                    <p>Doctores</p>
+                </div>
+                <div class="icon">
+                    <i class="ion fas bi bi-person-lines-fill"></i>
+                </div>
+                <a href="{{ url('admin/doctores') }}" class="small-box-footer">Más información<i class="fas bi bi-file-person"></i></a>
+            </div>
+        </div>
+
+        <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-secondary">
+                <div class="inner">
+                    <h3>{{ $total_horarios }}</h3>
+                    <p>Horarios</p>
+                </div>
+                <div class="icon">
+                    <i class="ion fas bi bi-calendar2-week"></i>
+                </div>
+                <a href="{{ url('admin/horarios') }}" class="small-box-footer">Más información<i class="fas bi bi-file-person"></i></a>
+            </div>
+        </div>
+
     </div>
 @endsection
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -158,6 +158,79 @@ scratch. This page gets rid of all links and provides the needed markup only.
                             </li>
                         </ul>
                     </li>
+
+                    <li class="nav-item">
+                        <a href="#" class="nav-link active">
+                            <i class="nav-icon fas bi bi-door-open"></i>
+                            <p>
+                                Consultorios
+                                <i class="right fas fa-angle-left"></i>
+                            </p>
+                        </a>
+                        <ul class="nav nav-treeview">
+                            <li class="nav-item">
+                                <a href="{{ url('admin/consultorios/create') }}" class="nav-link active">
+                                    <i class="far fa-circle nav-icon"></i>
+                                    <p>Creación de consultorios</p>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="{{ url('admin/consultorios') }}" class="nav-link active">
+                                    <i class="far fa-circle nav-icon"></i>
+                                    <p>Listado de consultorios</p>
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+
+                    <li class="nav-item">
+                        <a href="#" class="nav-link active">
+                            <i class="nav-icon fas bi bi-person-lines-fill"></i>
+                            <p>
+                                Doctores
+                                <i class="right fas fa-angle-left"></i>
+                            </p>
+                        </a>
+                        <ul class="nav nav-treeview">
+                            <li class="nav-item">
+                                <a href="{{ url('admin/doctores/create') }}" class="nav-link active">
+                                    <i class="far fa-circle nav-icon"></i>
+                                    <p>Creación de doctores</p>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="{{ url('admin/doctores') }}" class="nav-link active">
+                                    <i class="far fa-circle nav-icon"></i>
+                                    <p>Listado de doctores</p>
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+
+                    <li class="nav-item">
+                        <a href="#" class="nav-link active">
+                            <i class="nav-icon fas bi bi-calendar2-week"></i>
+                            <p>
+                                Horarios
+                                <i class="right fas fa-angle-left"></i>
+                            </p>
+                        </a>
+                        <ul class="nav nav-treeview">
+                            <li class="nav-item">
+                                <a href="{{ url('admin/horarios/create') }}" class="nav-link active">
+                                    <i class="far fa-circle nav-icon"></i>
+                                    <p>Creación de horarios</p>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="{{ url('admin/horarios') }}" class="nav-link active">
+                                    <i class="far fa-circle nav-icon"></i>
+                                    <p>Listado de horarios</p>
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+
                     <li class="nav-item">
                         <a href="#" class="nav-link" style="background-color: #a9200e">
                             <i class="nav-icon fas bi bi-door-closed"></i>
