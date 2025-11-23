25f9c2b594e5a0fbcb237c0b905c1c478f6ad5ee
Jhon Quiceno <quicenojale@gmail.com>
2025-11-22 19:18:27 -0500

    refactor: integrar RoleSeeder y HorarioSeeder en DatabaseSeeder - Ejecutar RoleSeeder y HorarioSeeder en proceso de seeding - Asegurar inicialización correcta de datos base

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Secretaria;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -14,41 +15,58 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        // Primero se deben crear los roles y permisos
+        $this->call([
+            RoleSeeder::class,
+        ]);
 
-//        User::factory()->create([
-//            'name' => 'Test User',
-//            'email' => 'test@example.com',
-//        ]);
-
+        // Luego se crean los usuarios con sus roles asignados
         User::create([
             'name' => 'Administrador',
             'email' => 'admin@admin.com',
             'password' => Hash::make('admin123'),
-        ]);
+        ])->assignRole('admin');
 
         User::create([
             'name' => 'Secretaria',
             'email' => 'secretaria@admin.com',
             'password' => Hash::make('admin123'),
+        ])->assignRole('secretaria');
+
+        Secretaria::create([
+            'nombres' => 'Secretaria',
+            'apellidos' => 'Miraflorez',
+            'identificacion' => '1234567890',
+            'telefono' => '0987654321',
+            'fecha_nacimiento' => '10/10/1990',
+            'direccion' => 'Calle Falsa 123',
+            'user_id' => '2'
         ]);
 
         User::create([
             'name' => 'Doctor',
             'email' => 'doctor@admin.com',
             'password' => Hash::make('admin123'),
-        ]);
+        ])->assignRole('doctor');
 
         User::create([
             'name' => 'Paciente',
             'email' => 'paciente@admin.com',
             'password' => Hash::make('admin123'),
-        ]);
+        ])->assignRole('paciente');
 
+        User::create([
+            'name' => 'Usuario',
+            'email' => 'usuario@admin.com',
+            'password' => Hash::make('admin123'),
+        ])->assignRole('usuario');
+
+        // Finalmente se cargan los otros seeders
         $this->call([
             ConsultorioSeeder::class,
             PacienteSeeder::class,
             DoctorSeeder::class,
+            HorarioSeeder::class,
         ]);
     }
 }
