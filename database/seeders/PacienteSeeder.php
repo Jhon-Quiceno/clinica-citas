7837a33427263fdd20bbf47c1624ec22dd752b42
Jhon Quiceno <quicenojale@gmail.com>
2025-11-22 19:19:44 -0500

    refactor: actualizar PacienteSeeder con datos mejorados - Generar pacientes con datos más realistas - Mejorar relaciones entre modelos - Asegurar integridad de datos

--- database/seeders/PacienteSeeder.php
+++ database/seeders/PacienteSeeder.php
@@ -13,6 +13,8 @@ class PacienteSeeder extends Seeder
      */
     public function run(): void
     {
-        Paciente::factory()->count(20)->create();
+        Paciente::factory()->count(20)->create()->each(function ($user){
+           $user->assignRole('paciente');
+        });
     }
 }
