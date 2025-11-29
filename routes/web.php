4a926fa58ac4c49f30bc5e8996c5f61c9a1aeee3
Jhon Quiceno <quicenojale@gmail.com>
2025-11-29 00:33:44 -0500

    feat: agregar rutas para funcionalidades AJAX de horarios - Remover rutas duplicadas o innecesarias - Mantener consistencia en definición de rutas - Optimizar estructura de archivo routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,6 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [App\Http\Controllers\WebController::class, 'index'])->name('index');
-//ajax
-Route::get('/consultorios/{id}', [App\Http\Controllers\WebController::class, 'cargar_datos_consultorios'])->name('cargar_datos_consultorios');
 
 Auth::routes();
 
@@ -78,3 +76,11 @@ Route::delete('/admin/horarios/{id}', [App\Http\Controllers\HorarioController::c
 //ajax
 Route::get('/admin/horarios/consultorios/{id}', [App\Http\Controllers\HorarioController::class, 'cargar_datos_consultorios'])->name('admin.horarios.cargar_datos_consultorios')->middleware('auth', 'can:admin.horarios.cargar_datos_consultorios');
 Route::get('/admin/horarios/doctores/consultorio/{id}', [App\Http\Controllers\HorarioController::class, 'cargar_doctores_consultorio'])->name('admin.horarios.cargar_doctores_consultorio')->middleware('auth', 'can:admin.horarios.cargar_doctores_consultorio');
+
+///rutas para el usuario
+//ajax
+Route::get('/consultorios/{id}', [App\Http\Controllers\WebController::class, 'cargar_datos_consultorios'])->name('cargar_datos_consultorios')->middleware('auth', 'can:cargar_datos_consultorios');;
+Route::get('/cargar_reserva_doctores/{id}', [App\Http\Controllers\WebController::class, 'cargar_reserva_doctores'])->name('cargar_reserva_doctores')->middleware('auth', 'can:cargar_reserva_doctores');;
+Route::get('/admin/ver_reservas/{id}', [App\Http\Controllers\AdminController::class, 'ver_reservas'])->name('ver_reservas')->middleware('auth', 'can:ver_reservas');;
+Route::post('/admin/eventos/create', [App\Http\Controllers\EventController::class, 'store'])->name('admin.eventos.create')->middleware('auth', 'can:admin.eventos.create');;
+Route::delete('/admin/eventos/destroy/{id}', [App\Http\Controllers\EventController::class, 'destroy'])->name('admin.eventos.destroy')->middleware('auth', 'can:admin.eventos.destroy');;
